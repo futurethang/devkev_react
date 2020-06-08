67fad172933f7009656545ec8ad905062d813eda
Kevin <kphyde@gmail.com>
2020-06-08 08:43:48 -0700

    Organize SASS modules

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import {
-  faEnvelope,
-  faEnvelopeSquare,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  Categories,
-  MainContentArea,
-  Post,
-  LogoTicker,
-} from "./components/index";
-import { useSpring, animated } from "react-spring";
-import { CATEGORY_KEYS } from "./utils/enums";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import { MainContentArea, LogoTicker, Home, Portfolio } from "./components/index";
 import "./assets/styles/App.scss";
 
 function App() {
-  
   return (
-    <div className="App">
-      {/* ///// HEADER CONTENT ///// */}
-      <header className="App-header">
-        <div className="title-nav">
-          <h1>DEV.KEV</h1>
-          {/* <Categories
+    <Router>
+      <div className="App">
+        {/* ///// HEADER CONTENT ///// */}
+        <header className="App-header">
+          <div className="title-nav">
+            <h1>DEV.KEV</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link to="/posts">Posts</Link>
+              </li>
+            </ul>
+
+            {/* <Categories
             categories={Object.keys(CATEGORY_KEYS)}
             filterCategory={filterCategory}
           /> */}
-        </div>
-        <div className="sub-header">
-          <h2>
-            online stuff,
-            <br />
-            offline stuff
-          </h2>
-        </div>
-      </header>
-      {/* ///// EVERYTHING BELOW THE HEADER ///// */}
-      <div className="site-content">
-        <Router>
-          <MainContentArea />
+          </div>
+          <div className="sub-header">
+            <h2>
+              online stuff,
+              <br />
+              offline stuff
+            </h2>
+          </div>
+        </header>
+        {/* ///// EVERYTHING BELOW THE HEADER ///// */}
+        <div className="site-content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/posts">
+              <MainContentArea />
+            </Route>
+          </Switch>
           {/* <main>
             {filteredPosts.posts.map((item, i) => {
               return (
@@ -51,56 +61,56 @@ function App() {
               );
             })}
           </main> */}
-        </Router>
-        <aside className="side-content">
-          {/* // Each of these might be iterations of an Aside component, what are their similarities? */}
-          <section className="about-me">
-            <h4 className="title is-size-2 has-text-white">Hi, I’m Kevin.</h4>
-            <h5 className="is-size-4 has-text-white">
-              I’m a Front End Developer & Designer.
-            </h5>
-            <p className="is-size-6 has-text-white">
-              I'm also an artist, musician, photographer, gardener, and kitchen
-              gnome. By day I tease apart complex UX problems with leading
-              JavaScript frameworks and chase the future with next-generation
-              web technnologies. My experience in design, business, customer
-              care, and leadership enable me to work excellently with designers
-              and devs alike to craft user experiences that are robust,
-              reusable, accessible, and creative.
-            </p>
-            <p className="is-size-6 has-text-white">
-              Looking for a strong addition to your team? &nbsp;
-              <a href="mailto:kphyde@gmail.com">Contact me!</a>
-            </p>
-            <a
-              className="social-logo"
-              href="https://github.com/futurethang"
-              target="blank"
-            >
-              <FontAwesomeIcon icon={faGithub} size="4x" />
-            </a>
-            <a
-              className="social-logo"
-              href="https://www.linkedin.com/in/kevin-hyde-fullstack/"
-              target="blank"
-            >
-              <FontAwesomeIcon icon={faLinkedin} size="4x" />
-            </a>
-            <a
-              className="social-logo"
-              href="mailto:kphyde@gmail.com"
-              target="blank"
-            >
-              <FontAwesomeIcon icon={faEnvelopeSquare} size="4x" />
-            </a>
-          </section>
-          <section className="technologies">
-            <h4>My Toolkit</h4>
-            <LogoTicker />
-          </section>
-        </aside>
+          <aside className="side-content">
+            {/* // Each of these might be iterations of an Aside component, what are their similarities? */}
+            <section className="about-me">
+              <h4 className="title is-size-2 has-text-white">Hi, I’m Kevin.</h4>
+              <h5 className="is-size-4 has-text-white">
+                I’m a Front End Developer & Designer.
+              </h5>
+              <p className="is-size-6 has-text-white">
+                I'm also an artist, musician, photographer, gardener, and
+                kitchen gnome. By day I tease apart complex UX problems with
+                leading JavaScript frameworks and chase the future with
+                next-generation web technnologies. My experience in design,
+                business, customer care, and leadership enable me to work
+                excellently with designers and devs alike to craft user
+                experiences that are robust, reusable, accessible, and creative.
+              </p>
+              <p className="is-size-6 has-text-white">
+                Looking for a strong addition to your team? &nbsp;
+                <a href="mailto:kphyde@gmail.com">Contact me!</a>
+              </p>
+              <a
+                className="social-logo"
+                href="https://github.com/futurethang"
+                target="blank"
+              >
+                <FontAwesomeIcon icon={faGithub} size="4x" />
+              </a>
+              <a
+                className="social-logo"
+                href="https://www.linkedin.com/in/kevin-hyde-fullstack/"
+                target="blank"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="4x" />
+              </a>
+              <a
+                className="social-logo"
+                href="mailto:kphyde@gmail.com"
+                target="blank"
+              >
+                <FontAwesomeIcon icon={faEnvelopeSquare} size="4x" />
+              </a>
+            </section>
+            <section className="technologies">
+              <h4>My Toolkit</h4>
+              <LogoTicker />
+            </section>
+          </aside>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import ArticleThumb from "./ArticleThumb";
 import Comment from "./Comment";
 import Post from "./Post";
+import Home from "./Home";
+import Portfolio from "./Portfolio";
 import MainContentArea from './MainContentArea';
 import MetaData from "./MetaData";
 import CategoryFilter from "./CategoryFilter"
 import Categories from "./Categories"
 import LogoTicker from "./LogoTicker"
 
-export { ArticleThumb, Categories, CategoryFilter, Comment, LogoTicker, Post, MainContentArea, MetaData };
+export { ArticleThumb, Home, Portfolio, Categories, CategoryFilter, Comment, LogoTicker, Post, MainContentArea, MetaData };
